cedbcf22d342c224eeec0383c17eebfab70c642f
jean-celeste <jeancarlotarrobago.celeste@bicol-u.edu.ph>
2024-05-04 00:46:59 +0800

    Added connect.php

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+   
+    //Create connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    $conn = new mysqli($servername, $username, $password);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    //Create database "bevi_db" if there is no existing database
+    $sql = "CREATE DATABASE IF NOT EXISTS bevi_db";
+    //Catches error creating databse
+    if (!$conn->query($sql)) {
+        echo "Error creating database: " . $conn->error;
+    }
+    
+?>
